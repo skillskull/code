a6d2a79ada86bb58a4c164687036fee1d48aaaaa
skillskull <rodnoriega13@gmail.com>
2022-03-31 14:42:53 -0600

    commit

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ root = Tk()
 #creating a  label widget
 
 myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="Rodrigo Noriega")
-
+myLabel2 = Label(root, text="Rodrigo N")
+myButton = Button(text="Close").grid(row=2, column=2)
 #shoving it into screen
 
 #myLabel.pack()
